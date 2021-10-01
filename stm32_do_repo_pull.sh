5122520c0aa171eeceeea6953ed510a896a59563
Bernardo Garza <bernardo.garza@hlioengineering.com>
2021-10-01 10:41:37 -0500

    Updated the do repo pull script to require to be run from the same directory where it exists.

--- stm32_do_repo_pull.sh
+++ stm32_do_repo_pull.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e # Make sure the script stops running when we hit an error
 
+# This is going to make it such that we can only run this script from the same directory. Well... Really it's only going
+#  to allow the script to run from directories that contain a script of the same name, but it's really unlikely that
+#  happens in somewhere other than the same directory this script lives in. This portion of code can be placed basically
+#  anywhere
+if ! $(grep --quiet -x $(basename $0) < <(ls)); then
+    echo "This script must be run from the directory where it exists"
+    exit 1
+fi
+
 # Make sure that we haven't already run the repo init command, we know this if the .repo directory has already been
 #  created
 if [ ! -d $PWD/.repo ]; then
