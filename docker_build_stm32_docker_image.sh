3a363cc559cde0632ac819868bf2055b4b4dacb7
Bernardo Garza <bernardo.garza@hlioengineering.com>
2021-10-01 10:40:57 -0500

    Updated the docker build image script to require that the shell script is being run from the directory where it exists

--- docker_build_stm32_docker_image.sh
+++ docker_build_stm32_docker_image.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-# Make sure that we go to the root of the repository to run this script
-git_toplevel_path=$(git rev-parse --show-toplevel)
-if [[ $git_toplevel_path == *"hcee-rcd-cont"* ]]; then
-    # At this point we are sure that we are running this bash script from the correct repository
-    if [[ $git_toplevel_path == *"yocto-dockerfiles"* ]]; then
-        # We are within the yocto-dockerfiles directory, take that into account
-        cd $git_toplevel_path/..
-    else
-        cd $git_toplevel_path
-    fi
-else
-    echo "This command MUST be run from within the hcee-rcd-cont repository directory"
-    exit 1 # Exit with an error code so that the rest of this script does run
-fi
-
-if [ ! -f $PWD/yocto-dockerfiles/README.md ]; then
-    # If the README.md file for the yocto-dockerfiles repository doesn't exist, it's because the submodule hasn't been
-    #  initialized yet. Run the command to initialize the submodules
-    git submodule update --init
+# This is going to make it such that we can only run this script from the same directory. Well... Really it's only going
+#  to allow the script to run from directories that contain a script of the same name, but it's really unlikely that
+#  happens in somewhere other than the same directory this script lives in. This portion of code can be placed basically
+#  anywhere
+if ! $(grep --quiet -x $(basename $0) < <(ls)); then
+    echo "This script must be run from the directory where it exists"
+    exit 1
 fi
 
 # At this point now we know that the submodule has been properly initialized and that we are in the root of the
