49706ff1cb02b20853d066b7b5dc787b0b6076a4
Bernardo Garza <bernardo.garza@hlioengineering.com>
2021-10-04 07:14:33 -0700

    Added a line in the docker setup yocto environment script to fix a bug in the configuration first generated by the envsetup.sh script for accessing an SState mirror url. Might put in a pull request to fix that, don't know yet...

--- docker/docker_stm32_setup_yocto_environment.sh
+++ docker/docker_stm32_setup_yocto_environment.sh
@@ -6,4 +6,9 @@
 # The environment variables for this script need to be setup in the docker run command before we get to this point
 source layers/meta-st/scripts/envsetup.sh < <(yes)
 
+# NOTE: BMG (Oct. 01, 2021) There is a bug in the envsetup.sh script that incorrectly writes the SSTATE_MIRRORS
+#  configuration for bitbake which was previously causing the url to not be correctly respected and the cache to not be
+#  downloaded
+sed -i 's#file:\/\/\\\.\\\*#file:\/\/\.\*#g' conf/site.conf 
+
 bitbake-layers add-layer ../layers/meta-st/meta-hlio-rcd/
